ea19c662013a13289ab470cb8286106998451e19
Vlad Zahiu <vladzahiu28@gmail.com>
2020-08-04 17:51:55 +0300

    Update interfata
    Arata mai bn

--- index.php
+++ index.php
@@ -10,49 +10,34 @@
     </head>
     <body>
         <div id="header">
-            <form class="middle-content" method="post" onsubmit="onSearchSubmit()"
-                  action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"
-                  id="headerForm" name="searchForm">
-                <a class="middle-v-aligned" id="header-home-link" href="index.php">Shop.ro</a>
-                <a class="middle-v-aligned" id="header-short-home-link" href="index.php">S</a>
-                <div id="right-header-container">
-                    <a class="middle-v-aligned" href="#" style="float: left; margin-right: 15px;">
-                        <span class="icon-user"></span>
-                        <span class="large-width-text">Contul meu</span>
+            <form action="search.php" method="post" name="searchForm"
+                  class="middle-content">
+                <a href="index.php" id="header-home-link"
+                   class="middle-v-aligned">Shop.ro</a>
+                   <div class="right-container middle-v-aligned">
+                    <!--php here-->
+                    <a href="#">
+                        <span class="icon icon-user"></span>
+                        <span class="mobile-hidden">Contul meu</span>
                     </a>
-                    <a class="middle-v-aligned" href="#" style="float: right">
-                        <span class="icon-cart"></span>
-                        <span class="large-width-text">Coșul meu</span>
+                    <a href="#">
+                        <span class="icon icon-cart"></span>
+                        <span class="mobile-hidden">Coșul meu</span>
                     </a>
                 </div>
-                <div id="middle-header-container">
-                    <div class="middle-v-aligned" id="header-input-wrapper">
-                        <button title="Căutare" class="middle-v-aligned" type="submit" name="searchSubmit">
-                            <span class="icon-search"></span></button>
-                        <input type="text" name="searchVal" id="searchField"
-                               placeholder="Descrie produsul căutat..." 
-                               onfocus="onFieldFocus()" onblur="onFieldFocus()" />
-                    </div>
+                <div class="middle-container middle-v-aligned">
+                    <button type="submit" name="searchSub">
+                        <span class="icon-search"></span>
+                    </button>
+                    <input type="text" name="searchVal"
+                           placeholder="Descrie produsul căutat..." />
+                    
                 </div>
+                
             </form>
         </div>
-        <div id="adv-search-content">
-            <div id="inner-search-content">
-                <div class="card">
-                    <span class="label-span">Categorie:</span>
-                    <hr />
-                </div>
-                <div class="card">
-                    
-                </div>
-                <div class="card">
-                    
-                </div>
-                test
-            </div>
+        <div id="main-content">
+            
         </div>
-        <button title="Căutare avansată" onclick="expandAction()" id="adv-search-button">
-            <span class="icon-chevron-down"></span>
-        </button>
     </body>
 </html>
