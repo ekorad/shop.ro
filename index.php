be5b68e394efdac09dd9442d32920f0261c256fc
Vlad Zahiu <vladzahiu28@gmail.com>
2020-08-11 14:12:59 +0300

    actualizat interfata grafica pentru mobile

--- index.php
+++ index.php
@@ -10,11 +10,26 @@ require('./includes/connect.php');
         <link rel="stylesheet" type="text/css" href="css/generic.css" />
         <link rel="stylesheet" type="text/css" href="css/index.css" />
         <script type="text/javascript" src="scripts/search.js"></script>
+        <script>
+            setInterval(function() {
+                if (window.innerWidth > 1300) {
+                    var menu = document.getElementById("nav");
+                    if (menu.classList.contains("expanded")) {
+                        mobileMenuButtonAction();
+                    }
+                }
+            }, 16);
+        </script>
     </head>
     <body>
+        <div id="overlay" onclick="mobileMenuButtonAction()"></div>
         <div id="header">
             <form action="search.php" method="post" name="searchForm"
                   class="middle-content">
+                <a class="middle-v-aligned" id="mobile-menu-button" 
+                   href="javascript:void(0)" onclick="mobileMenuButtonAction()">
+                    <span class="icon-menu"></span>
+                </a>
                 <a href="index.php" id="header-home-link"
                    class="middle-v-aligned">Shop.ro</a>
                 <div class="right-container middle-v-aligned">
@@ -38,12 +53,67 @@ require('./includes/connect.php');
             </form>
         </div>
         <div id="nav">
-
+            <ul class="middle-content">
+                <li>
+                    <a class="btn-expand" href="javascript:void(0)" onclick="navExpandAction(this)">Laptopuri</a>
+                    <div class="dropwdown-content">
+                        <a href="#">Notebook-uri</a>
+                        <a href="#">Ultrabook-uri</a>
+                    </div>
+                </li>
+                <li>
+                    <a class="btn-expand"  href="javascript:void(0)" onclick="navExpandAction(this)">Mobile</a>
+                    <div class="dropwdown-content">
+                        <a href="#">Telefoane</a>
+                        <a href="#">Tablete</a>
+                    </div>
+                </li>
+                <li>
+                    <a class="btn-expand"  href="javascript:void(0)" onclick="navExpandAction(this)">Componente</a>
+                    <div class="dropwdown-content">
+                        <a href="#">Plăci video</a>
+                        <a href="#">Plăci de bază</a>
+                        <a href="#">Procesoare</a>
+                        <a href="#">Memorii</a>
+                        <a href="#">SSD</a>
+                        <a href="#">HDD</a>
+                        <a href="#">Surse</a>
+                        <a href="#">Coolere</a>
+                    </div>
+                </li>
+                <li>
+                    <a class="btn-expand"  href="javascript:void(0)" onclick="navExpandAction(this)">Televizoare</a>
+                    <div class="dropwdown-content">
+                        <a href="#">Televizoare LED</a>
+                    </div>
+                </li>
+                <li>
+                    <a class="btn-expand"  href="javascript:void(0)" onclick="navExpandAction(this)">Imprimare</a>
+                    <div class="dropwdown-content">
+                        <a href="#">Imprimante</a>
+                        <a href="#">Cartușe</a>
+                    </div>
+                </li>
+                <li>
+                    <a class="btn-expand"  href="javascript:void(0)" onclick="navExpandAction(this)">Audio/Video</a>
+                    <div class="dropwdown-content">
+                        <a href="#">Căști</a>
+                        <a href="#">Boxe</a>
+                        <a href="#">Camere video</a>
+                    </div>
+                </li>
+                <li>
+                    <a class="btn-expand"  href="javascript:void(0)" onclick="navExpandAction(this)">Rețelistică</a>
+                    <div class="dropwdown-content">
+                        <a href="#">Routere Wireless</a>
+                        <a href="#">Switch-uri</a>
+                        <a href="#">Plăci rețea</a>
+                    </div>
+                </li>
+                <li><a href="#">Contact</a></li>
+            </ul>
         </div>
         <div id="main-content">
-            <?php
-                $sql = "SELECT DISTINCT `category` FROM `products`";
-            ?>
         </div>
     </body>
 </html>
--- scripts/search.js
+++ scripts/search.js
@@ -7,13 +7,32 @@ function onFieldFocus() {
     searchElemContainer.classList.toggle("focused");    
 }
 
-function expandAction() {
-    var button = document.getElementById("adv-search-button").childNodes[1];
-    var content = document.getElementById("adv-search-content");
-    if (button.classList.contains("toggled")) {
-        content.style.maxHeight = "10px";
-    } else {
-        content.style.maxHeight = content.scrollHeight + "px";
+function navExpandAction(btn) {
+    if (window.innerWidth > 1300) {
+        return;
     }
-    button.classList.toggle("toggled");
+    var expandableDiv = btn.nextSibling.nextSibling;
+    if (btn.classList.contains("toggled")) {
+        expandableDiv.style.maxHeight = "0px";
+    } else {
+        expandableDiv.style.maxHeight = expandableDiv.scrollHeight + "px";
+    }
+    btn.classList.toggle("toggled");
+}
+
+function mobileMenuButtonAction() {
+    var button = document.getElementById("mobile-menu-button");
+    var menu = document.getElementById("nav");
+    var expandBtns = document.getElementsByClassName("btn-expand");
+    var overlay = document.getElementById("overlay");
+    menu.classList.toggle("expanded");
+    overlay.classList.toggle("expanded");
+    if (!menu.classList.contains("expanded")) {
+        var i;
+        for (i = 0; i < expandBtns.length; i++) {
+            if (expandBtns[i].classList.contains("toggled")) {
+                navExpandAction(expandBtns[i]);
+            }
+        }
+    }    
 }
